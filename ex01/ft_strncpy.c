70775655a101c9862c556508ffb1a08413d1fb7b
Robin COLLAS <rcollas@e3r4p22.42.fr>
2021-03-21 11:51:38 +0100

    chopin in c minor

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 07:54:38 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/21 08:51:56 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/21 11:45:44 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	i = -1;
-	while (src[++i] && i < n)
+	i = 0;
+	while (src[i] && i < n)
+	{
 		dest[i] = src[i];
-	i--;
-	while (i++ < n)
-		dest[i] = '\0';
+		i++;
+	}
+	while (i <= n)
+		dest[i++] = '\0';
 	return (dest);
 }
--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,18 +6,21 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 13:16:37 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/21 08:57:23 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/21 11:47:39 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int i;
+	unsigned int i;
 
-	i = -1;
-	while (++i + 1 < size && src[i])
+	i = 0;
+	while (i + 1 < size && src[i])
+	{
 		dest[i] = src[i];
+		i++;
+	}
 	if (size > 0)
-		dest[i] = 0;
+		dest[i] = '\0';
 	return (i);
 }
