08f3591a4e2adf4e11dd3334a89959e0f6a4dd05
Robin COLLAS <rcollas@e3r4p22.42.fr>
2021-03-22 07:56:36 +0100

    final ex01 ex10

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,11 +6,9 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 07:54:38 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/21 11:45:44 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/22 07:54:55 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
@@ -22,7 +20,7 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-	while (i <= n)
+	while (i < n)
 		dest[i++] = '\0';
 	return (dest);
 }
--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,9 +6,21 @@
 /*   By: rcollas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 13:16:37 by rcollas           #+#    #+#             */
-/*   Updated: 2021/03/21 11:47:39 by rcollas          ###   ########.fr       */
+/*   Updated: 2021/03/22 07:51:30 by rcollas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int i;
+
+	i = -1;
+	while (str[++i])
+	{
+		continue;
+	}
+	return (i);
+}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
@@ -22,5 +34,5 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	}
 	if (size > 0)
 		dest[i] = '\0';
-	return (i);
+	return (ft_strlen(src));
 }
